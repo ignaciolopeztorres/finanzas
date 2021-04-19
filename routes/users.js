b4195371f9f9471fb8439876f2a20d1e0b3f37e9
Ignacio Lopez Torres <ignacio.lopeztorres@hotmail.com>
2021-04-18 22:10:57 -0500

    [Add] mysql agregado
     Paquete mysql agregado

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/signup', (req, res, next)=>{
+  const object = { name : req.body.name,
+    lastname : req.body.lastname, 
+    username : req.body.username, 
+    email : req.body.email, 
+    password : req.body.password
+  }
+  console.log(object);
+  res.send('hello');
+});
+
 module.exports = router;
