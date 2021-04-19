64b55457f7229aa7e91f84e3ee8814205f181e95
Ignacio Lopez Torres <ignacio.lopeztorres@hotmail.com>
2021-04-18 22:45:49 -0500

    [feat] Conexion con el servidor de base de datos

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const conexion = require('./../database/mysql');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+conexion.query('SELECT 1 + 1 AS solution', (error, results, fields)=>{
+  if (error) {
+    throw error;
+  }
+  console.log('the solution is: ', results[0].solution);
+});
 module.exports = router;
