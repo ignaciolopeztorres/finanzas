910142e4f2c05179cae728fd86bd37fe2e5094e5
Ignacio Lopez Torres <ignacio.ujat@gmail.com>
2025-10-27 13:19:38 -0600

    [add] Crea el modelo de ingresos
    Crea el modelo para la estructura de la tabla

---
+++ models/income.model.js
@@ -0,0 +1,42 @@
+//ingresos
+/**
+ * Modelo de ingresos
+ * - amount: Monto
+ * - source: furnte de ingreso
+ * - recivedAt: fecha de recibo
+ * - notes: notas adicionales
+ */
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../core/config/sequelize.config.js";
+
+class Income extends Model{}
+
+Income.init({
+    idIncome: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    source: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    receivedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    notes: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+}, {
+    timestamps: true,
+    sequelize,
+    modelName: "Income"
+});
+export default Income;
